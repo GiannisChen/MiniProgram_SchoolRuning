96012e6a552126791744a6436972b609fda0b4af
GiannisChen <1065619715@qq.com>
2020-01-16 14:50:58 +0800

    static map test
    静态地图的载入

--- pages/location/location.js
+++ pages/location/location.js
@@ -1,18 +1,47 @@
 // pages/location/location.js
+
+/**
+ * 实例化AMapWX对象，处理搜索数据
+ */
+var amapFile = require('../../libs/amap-wx.js');
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    src: ''
+    
   },
+  onLoad: function() {
+    var that = this;
+    var myAmapFun = new amapFile.AMapWX({ key:"0b72990a6f111479ca016c9e11457b06"});
+    wx.getSystemInfo({
+      success: function(data){
+        var height = data.windowHeight;
+        var width = data.windowWidth;
+        var size = width + "*" + height;
+        myAmapFun.getStaticmap({
+          zoom: 17,
+          size: size,
+          scale: 2,
+          markers: "mid,0xFF0000,A:116.37359,39.92437;116.47359,39.92437",
+         
+          success: function(data){
+            that.setData({
+              src: data.url
+            })
+            
+          },
+          fail: function(info){
+            wx.showModal({title:info.errMsg})
+          }
+        })
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+      }
+    })
+    
   },
 
   /**
@@ -62,23 +91,5 @@ Page({
    */
   onShareAppMessage: function () {
 
-  },
-
-  /**
-   * 获取位置信息
-   */
-  getLocation:function() {
-    var that=this;
-    wx.getLocation({
-      success: function(res) {
-        that.setData({
-          lat:res.latitude,
-          lon:res.longitude,
-          speed:res.speed,
-          accuracy:res.accuracy
-        })
-      },
-    })
   }
-
 })
